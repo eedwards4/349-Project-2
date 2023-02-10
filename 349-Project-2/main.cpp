f7ee56f8038240d572336fc23a30bffee96997b2
Ethan Edwards <124391459+eedwards4@users.noreply.github.com>
2023-02-10 11:57:51 -0800

    Initial commit

---
+++ 349-Project-2/main.cpp
@@ -0,0 +1,24 @@
+/*
+Ethan Edwards, Joe Coon
+   CS 349: Problem Solving in a Team Programming Environment
+       --> Taught by Dr. Gurman Gill; Sonoma State University
+ */
+
+#include <chrono> // https://en.cppreference.com/w/cpp/chrono/hh_mm_ss
+
+float speedTester() {
+
+}
+
+string calcTime(int sp, int dist){
+    (float)sp / dist;
+}
+
+int main(){
+    string timediff;
+    // File input stuff
+    timediff = calcTime ()
+
+    // Logic for organizing output
+    outstream << timediff
+}
