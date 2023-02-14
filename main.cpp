a84f2affa44b85b7e7d00da187ba211bcb27222a
Joe Coon <jwbcoon@gmail.com>
2023-02-14 00:10:18 -0800

    Added initializeFiles function that either accepts user arguments for I/O files or prompts users for I/O filenames. Only current issue is infinite looping if the user has no input file to provide.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 Ethan Edwards, Joe Coon
    CS 349: Problem Solving in a Team Programming Environment
        --> Taught by Dr. Gurman Gill; Sonoma State University
+
+ "Speed Can Cost You" Program.
  */
 
 #include <chrono> // https://en.cppreference.com/w/cpp/chrono/hh_mm_ss
@@ -13,29 +15,36 @@ using namespace std;
 
 string calcTimeDiff(int sp1, int sp2, int dist);
 float convertUnits(float convTime);
-string timeFixer(string timeRaw);
+string timeFormatter(string timeRaw);
+void initializeFiles(fstream& inFile, fstream& outFile, string& fname, int argc, const char* argv[]);
 
-int main(){
+
+/*
+ * Process input data on driver speeds into calculated travel time differences and
+ * output it to a file.
+ */
+int main(int argc, const char* argv[]){
     int speed1 = 1, speed2 = 1, distance = 1;
-    fstream inFile("tinput.txt"), outFile("tout.txt");
-    string timeDiff;
+    fstream inFile, outFile;
+    string timeDiff, fname;
 
-    // File input stuff
+    initializeFiles(inFile, outFile, fname, argc, argv);
+
     if (inFile.is_open()) {
-        cout << "File is open" << endl;
+        cout << "Opening " << fname  << "..." << endl;
         while (speed1 + speed2 + distance != 0) {
             inFile >> distance;
             inFile >> speed1;
             inFile >> speed2;
             inFile.ignore();
-            timeDiff = calcTimeDiff(speed1, speed2, distance);
 
-            timeDiff = timeFixer(timeDiff); // Fix time formatting
-
-            // Logic for organizing output
-            outFile << timeDiff << endl;
-            cout << timeDiff << endl;
+            if (speed1 + speed2 + distance != 0) {
+                timeDiff = calcTimeDiff(speed1, speed2, distance);
+                timeDiff = timeFormatter(timeDiff); // Fix time formatting
+                outFile << timeDiff << endl;
+            }
         }
+        cout << "Finished." << endl;
         inFile.close();
         outFile.close();
     }
@@ -85,7 +94,7 @@ float convertUnits(float convTime) {
  * Convert from float time to int time, and add leading 0s if necessary.
  * timeRaw must be a string formatted with floats in "h:m:s" form.
  */
-string timeFixer(string timeRaw){
+string timeFormatter(string timeRaw){
     string h, m, s;
     char sep = ':';
 
@@ -99,4 +108,32 @@ string timeFixer(string timeRaw){
     if (stoi(s) < 10){s = "0" + s;}
 
     return h + sep + m + sep + s;
+}
+
+
+/*
+ *  Manages file initialization by detecting program arguments and accordingly using supplied files
+ *  or prompting the user for sufficient files to execute the rest of the program.
+ *
+ *  argv[1] is expected to contain an input filepath. argv[2] is expected to contain an output filepath.
+ *  fname, inFile, and outFile are all expected to be declared, uninitialized variables.
+ */
+void initializeFiles(fstream& inFile, fstream& outFile, string& fname, int argc, const char* argv[]) {
+    if (argc == 3) {
+        fname = argv[1];
+        inFile.open(argv[1]);
+        outFile.open(argv[2]);
+    }
+    while (!outFile.is_open()) {
+        cout << "Please provide an existing filename to overwrite, or a new filename to create: ";
+        cin >> fname;
+        outFile = fstream(fname, ios::out); /*invoking the fstream constructor will create a new file
+                                              if one does not already exist.*/
+        fname.clear();
+    }
+    while (!inFile.is_open()) {
+        cout << "No input file was found. Please provide a relative path and filename for an input file: ";
+        cin >> fname;
+        inFile.open(fname);
+    }
 }
